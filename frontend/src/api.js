6db7c2cf3cf2f9b582fc16526dbdd3fd3a67c42d
vaibhavbudhia <vaibhavbudhia7306@gmail.com>
2026-05-02 18:10:34 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://new-parking-system-production.up.railway.app/api' });
 
 API.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
